045c30fa120f7555168b47f9b85efa8f922c0856
Jason <bjason@gmail.com>
2014-02-03 14:47:14 -0800

    resize example code.. awesome..

--- get.js
+++ get.js
@@ -1,4 +1,7 @@
 var page = require('webpage').create();
+
+page.viewportSize = { width: 1200, height: 1600 };
+
 page.open('http://sqor.com', function() {
   setTimeout(function(){
 	  page.render('example.png');
